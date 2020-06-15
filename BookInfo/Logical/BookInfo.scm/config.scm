e5e50643c1f192684503105d845a000475326400
virtualmonster <swalker.home@gmail.com>
2020-06-15 21:46:28 +0100

    first upload

---
+++ BookInfo/Logical/BookInfo.scm/config.scm
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<editableResource createdTimestamp="1591711543324"
+    createdUser="STUART.WALKER" id="-47ea204f:172995c51ea:-7e20"
+    type="service_component_resource" updatedTimestamp="1591711543324"
+    updatedUser="STUART.WALKER" version="1.1">
+    <loggingConfiguration debug="false" writeStubEventsToDB="false"/>
+</editableResource>
